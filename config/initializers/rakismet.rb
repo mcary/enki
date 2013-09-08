4857a90fb11ee2c4a21afca05a0c99786ad47943
Marcel M. Cary <marcel@oak.homeunix.org>
2013-09-07 20:18:23 -0700

    Make akismet and rspec configs work for Ruby 2.0

--- config/initializers/rakismet.rb
+++ config/initializers/rakismet.rb
@@ -1,5 +1,5 @@
 enki_config = YAML.load(IO.read("#{Rails.root}/config/enki.yml")).symbolize_keys
 rakismet_config = Rails.application.config.rakismet
-rakismet_config.key  ||= enki_config[:rakismet]['key']
-rakismet_config.host ||= enki_config[:rakismet]['host']
-rakismet_config.url  ||= enki_config[:rakismet]['url'] || enki_config[:url]
+rakismet_config[:key]  ||= enki_config[:rakismet]['key']
+rakismet_config[:host] ||= enki_config[:rakismet]['host']
+rakismet_config[:url]  ||= enki_config[:rakismet]['url'] || enki_config[:url]
